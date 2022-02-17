520225a741dd67246ebc317d18538d929e3c75d9
Holm Hinrichs <hinrichs@ponton.de>
2022-02-17 17:32:36 +0100

    Adding backend

---
+++ consumption_tracker/src/main/java/de/holm/consumptionTracker/ConsumptionTrackerApplication.java
@@ -0,0 +1,13 @@
+package de.holm.consumptionTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumptionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumptionTrackerApplication.class, args);
+	}
+
+}
