f6c441048490583302b5298de4822a8e22e824bf
Holm Hinrichs <hinrichs@ponton.de>
2022-02-21 00:38:09 +0100

    adding actions

--- src/main/java/de/holm/consumptionTracker/ConsumptionTrackerApplication.java
+++ src/main/java/de/holm/consumptionTracker/ConsumptionTrackerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
@@ -20,6 +21,16 @@ public class ConsumptionTrackerApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**");
+			}
+		};
+	}
+
+	@Bean
+	public WebMvcConfigurer webHandler(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addResourceHandlers(ResourceHandlerRegistry registry) {
+				registry.addResourceHandler("/**").addResourceLocations("classpath:/public/");
 			}
 		};
 	}
