064910ae2350c410fd507dbebf4a732e621c78a1
Holm Hinrichs <holm.hinrichs@t-online.de>
2022-03-06 00:46:58 +0100

    wip on dashboard

--- consumption_tracker_ui/src/views/home/Home.vue
+++ consumption_tracker_ui/src/views/home/Home.vue
@@ -1,4 +1,4 @@
-<template src="./Home.html"></template>
-
+<template src="./Overview.html"></template>
+<style lang="scss" src="./home.scss"></style>
 <script lang="ts" src="./Home.ts"></script>
 
--- src/main/java/de/holm/consumptionTracker/ConsumptionTrackerApplication.java
+++ src/main/java/de/holm/consumptionTracker/ConsumptionTrackerApplication.java
@@ -3,35 +3,52 @@ package de.holm.consumptionTracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class ConsumptionTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumptionTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumptionTrackerApplication.class, args);
+    }
 
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer(){
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 
-	@Bean
-	public WebMvcConfigurer webHandler(){
-		return new WebMvcConfigurer() {
-			@Override
-			public void addResourceHandlers(ResourceHandlerRegistry registry) {
-				registry.addResourceHandler("/**").addResourceLocations("classpath:/public/");
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer webHandler() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/**")
+                        .addResourceLocations("classpath:/public/")
+                        .resourceChain(true)
+                        .addResolver(new PathResourceResolver(){
+                            // Resetting to main to let VUE Js take over routing
+                            @Override
+                            protected Resource getResource (String resourcePath, Resource location) throws IOException {
+                                Resource requestedResource = location.createRelative(resourcePath);
+
+                                return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                        : new ClassPathResource("/public/index.html");
+                            }
+                        });
+            }
+        };
+    }
 }
